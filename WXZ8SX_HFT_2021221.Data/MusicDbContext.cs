6ebc8f3b01e7dec36bc1c99f33d43eb124890573
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-10-16 14:37:46 +0200

    Packages: Microsoft.EntityFrameworkCore / Microsoft.EntityFrameworkCore.SqlServer / Microsoft.EntityFrameworkCore.Proxies / Microsoft.EntityFrameworkCore.Tools were installed in th Data Layer

---
+++ WXZ8SX_HFT_2021221.Data/MusicDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WXZ8SX_HFT_2021221.Data
+{
+    public class MusicDbContext : DbContext
+    {
+
+    }
+}
