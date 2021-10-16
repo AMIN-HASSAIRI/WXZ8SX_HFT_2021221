1461044b2cb3db36263974f2bd3c7aa5283ef17a
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-10-16 14:09:23 +0200

    Genre Class was created with content in the Models Layer

---
+++ WXZ8SX_HFT_2021221.Models/Genre.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WXZ8SX_HFT_2021221.Models
+{
+    [Table("Genre")]
+    public class Genre
+    {
+        [Key]
+        public int GenreId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string GenreName { get; set; }
+    }
+}
