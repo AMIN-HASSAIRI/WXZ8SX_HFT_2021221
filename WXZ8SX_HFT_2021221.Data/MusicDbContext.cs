ec22f62745959e449ad8539297ed089dd8dd6ab5
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-10-16 14:42:22 +0200

    Packages: Microsoft.EntityFrameworkCore was installed in the Models Layer

--- WXZ8SX_HFT_2021221.Data/MusicDbContext.cs
+++ WXZ8SX_HFT_2021221.Data/MusicDbContext.cs
@@ -4,11 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WXZ8SX_HFT_2021221.Models;
 
 namespace WXZ8SX_HFT_2021221.Data
 {
     public class MusicDbContext : DbContext
     {
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Song> Songs { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseLazyLoadingProxies().UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=C:\USERS\AMIN\SOURCE\REPOS\PROJECT_TEST\DATALAYER\DBM.MDF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Seed();
+        }
     }
 }
