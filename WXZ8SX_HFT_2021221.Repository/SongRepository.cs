3106f5f7205568aec312bffd4a3e03da0b97f027
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-10-20 12:12:31 +0200

    SongRepository Class was created in the Repository Layer with GetOne method

---
+++ WXZ8SX_HFT_2021221.Repository/SongRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WXZ8SX_HFT_2021221.Data;
+using WXZ8SX_HFT_2021221.Models;
+
+namespace WXZ8SX_HFT_2021221.Repository
+{
+    public class SongRepository : Repository<Song>, ISongRepository
+    {
+        public SongRepository(MusicDbContext musicDbContext) : base(musicDbContext)
+        {
+        }
+
+        public override Song GetOne(int id)
+        {
+            return MusicDbContext.Songs.SingleOrDefault(song => song.SongId == id);
+        }
+    }
+}
