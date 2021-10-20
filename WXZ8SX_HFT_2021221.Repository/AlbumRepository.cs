5bd455f3e4e3f5dfd2de69caf78812cbd59cc348
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-10-20 11:54:36 +0200

    Album Class was created in the Repository Layer having a GetOne method

---
+++ WXZ8SX_HFT_2021221.Repository/AlbumRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WXZ8SX_HFT_2021221.Data;
+using WXZ8SX_HFT_2021221.Models;
+
+namespace WXZ8SX_HFT_2021221.Repository
+{
+    public class AlbumRepository : Repository<Album>, IAlbumRepository
+    {
+        public AlbumRepository(MusicDbContext musicDbContext) : base(musicDbContext)
+        {
+        }
+
+        public override Album GetOne(int id)
+        {
+            return MusicDbContext.Albums.SingleOrDefault(album => album.AlbumId == id);
+        }
+    }
+}
