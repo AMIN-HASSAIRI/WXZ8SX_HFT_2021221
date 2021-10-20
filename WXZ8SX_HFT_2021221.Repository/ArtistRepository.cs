3febb4b33e9a466d83568b3a6498e156188b173a
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-10-20 12:08:07 +0200

    ArtistRepository Class was created with GetOne method in the Repository Layer

---
+++ WXZ8SX_HFT_2021221.Repository/ArtistRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WXZ8SX_HFT_2021221.Data;
+using WXZ8SX_HFT_2021221.Models;
+
+namespace WXZ8SX_HFT_2021221.Repository
+{
+    public class ArtistRepository : Repository<Artist>, IArtistRepository
+    {
+        public ArtistRepository(MusicDbContext musicDbContext) : base(musicDbContext)
+        {
+        }
+
+        public override Artist GetOne(int id)
+        {
+            return MusicDbContext.Artists.SingleOrDefault(artist => artist.ArtistId == id);
+        }
+    }
+}
