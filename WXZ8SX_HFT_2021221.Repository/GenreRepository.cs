e9dc818fe3f3190c75472c9f2c0d0bed2c67b64c
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-10-20 12:02:01 +0200

    Genre Class was created in the Repository Layer with GetOne method

---
+++ WXZ8SX_HFT_2021221.Repository/GenreRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WXZ8SX_HFT_2021221.Data;
+using WXZ8SX_HFT_2021221.Models;
+
+namespace WXZ8SX_HFT_2021221.Repository
+{
+    public class GenreRepository : Repository<Genre>, IGenreRepository
+    {
+        public GenreRepository(MusicDbContext musicDbContext) : base(musicDbContext)
+        {
+        }
+
+        public override Genre GetOne(int id)
+        {
+            return MusicDbContext.Genres.SingleOrDefault(genre => genre.GenreId == id);
+        }
+    }
+}
