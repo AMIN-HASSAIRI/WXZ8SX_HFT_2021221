71d499d77fcb9c878c443cc681e064e23bcf06cc
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-05 18:51:04 +0100

    CreateSong() was implemented in the SongLogic

--- WXZ8SX_HFT_2021221.Logic/SongLogic.cs
+++ WXZ8SX_HFT_2021221.Logic/SongLogic.cs
@@ -12,6 +12,20 @@ namespace WXZ8SX_HFT_2021221.Logic
     {
         protected readonly ISongRepository _songRepository;
 
+        public void CreateSong(int songId, string songName, double songLength, string writer, string singer, int albumId)
+        {
+            Song newSong = new Song
+            {
+                SongId = songId,
+                Name = songName,
+                Length = songLength,
+                Writer = writer,
+                Singer = singer,
+                AlbumId = albumId
+            };
+            _songRepository.Add(newSong);
+        }
+
         public Song GetSong(int songId)
         {
             return _songRepository.GetOne(songId);
