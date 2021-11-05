7ca042a924cf45e83da624c52dd7e982e7d24545
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-05 18:09:57 +0100

    GetArtist() was implemented in the ArtistLogic

--- WXZ8SX_HFT_2021221.Logic/ArtistLogic.cs
+++ WXZ8SX_HFT_2021221.Logic/ArtistLogic.cs
@@ -12,6 +12,19 @@ namespace WXZ8SX_HFT_2021221.Logic
     {
         private readonly IArtistRepository _artistRepository;
 
+        public Artist GetArtist(int artistId)
+        {
+            Artist artist = _artistRepository.GetOne(artistId);
+            if (artist == null)
+            {
+                throw new Exception("This artist ID does not exists!");
+            }
+            else
+            {
+                return artist;
+            }
+        }
+
         public List<Artist> GetArtists()
         {
             return _artistRepository.GetAll().ToList();
--- WXZ8SX_HFT_2021221.Logic/IArtistLogic.cs
+++ WXZ8SX_HFT_2021221.Logic/IArtistLogic.cs
@@ -11,5 +11,7 @@ namespace WXZ8SX_HFT_2021221.Logic
     {
         List<Artist> GetArtists();
 
+        Artist GetArtist(int artistId);
+
     }
 }
