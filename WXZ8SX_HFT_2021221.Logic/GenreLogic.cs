85a41005f3bcb1d0b5458ceecb856c917c88664b
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-05 17:42:40 +0100

    GetGenres() was implemented in the GenreLogic

---
+++ WXZ8SX_HFT_2021221.Logic/GenreLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WXZ8SX_HFT_2021221.Models;
+using WXZ8SX_HFT_2021221.Repository;
+
+namespace WXZ8SX_HFT_2021221.Logic
+{
+    public class GenreLogic : IGenreLogic
+    {
+        private readonly IGenreRepository _genreRepository;
+
+        public List<Genre> GetGenres()
+        {
+            return _genreRepository.GetAll().ToList();
+        }
+    }
+}
