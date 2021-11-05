e015b87dc01a63b3b1a13ef2f79e65fe99f0b242
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-05 18:42:14 +0100

    GetSongs() was implemented in the SongLogic

---
+++ WXZ8SX_HFT_2021221.Logic/SongLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WXZ8SX_HFT_2021221.Models;
+using WXZ8SX_HFT_2021221.Repository;
+
+namespace WXZ8SX_HFT_2021221.Logic
+{
+    public class SongLogic : ISongLogic
+    {
+        protected readonly ISongRepository _songRepository;
+
+        public List<Song> GetSongs()
+        {
+            return _songRepository.GetAll().ToList();
+        }
+    }
+}
