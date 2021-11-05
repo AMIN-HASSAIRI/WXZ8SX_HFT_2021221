2458aff06cd2f3c7ba4f8a2643220e1c923d842e
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-05 16:31:40 +0100

    IAlbumLogic interface was created with the first method GetAlbums()

--- WXZ8SX_HFT_2021221.Data/MusicDbContext.cs
+++ WXZ8SX_HFT_2021221.Data/MusicDbContext.cs
@@ -18,7 +18,7 @@ namespace WXZ8SX_HFT_2021221.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
 
-            optionsBuilder.UseLazyLoadingProxies().UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Amin\source\repos\WXZ8SX_HFT_2021221\WXZ8SX_HFT_2021221.Data\DB.mdf;Integrated Security=True");
+            optionsBuilder.UseLazyLoadingProxies().UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DB.mdf;Integrated Security=True");
 
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -26,11 +26,11 @@ namespace WXZ8SX_HFT_2021221.Data
             base.OnModelCreating(modelBuilder);
             modelBuilder.Seed();
 
-            modelBuilder.Entity<Artist>().HasData(
-                new Artist 
-                { 
-                    ArtistId = 0, ArtistName = "", DateOfBirth = DateTime.Parse("00/00/0000"), NumberOfAlbums = 0
-                });
+            //modelBuilder.Entity<Artist>().HasData(
+            //    new Artist 
+            //    { 
+            //        ArtistId = 0, ArtistName = "", DateOfBirth = DateTime.Parse("00/00/0000"), NumberOfAlbums = 0
+            //    });
         }
     }
 }
---
+++ WXZ8SX_HFT_2021221.Logic/IAlbumLogic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WXZ8SX_HFT_2021221.Models;
+
+namespace WXZ8SX_HFT_2021221.Logic
+{
+    public interface IAlbumLogic
+    {
+        List<Album> GetAlbums();
+    }
+}
