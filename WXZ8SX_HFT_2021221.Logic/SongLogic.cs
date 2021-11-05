e25f4b9ecd9a295e35e72e2fd90e3e0127a5a5e1
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-05 19:06:29 +0100

    GetDateOfBirthOfSinger() was implemented in the SongLogic

--- WXZ8SX_HFT_2021221.Logic/SongLogic.cs
+++ WXZ8SX_HFT_2021221.Logic/SongLogic.cs
@@ -11,6 +11,8 @@ namespace WXZ8SX_HFT_2021221.Logic
     public class SongLogic : ISongLogic
     {
         protected readonly ISongRepository _songRepository;
+        protected readonly IAlbumRepository _albumRepository;
+        protected readonly IArtistRepository _artistRepository;
 
         public void CreateSong(int songId, string songName, double songLength, string writer, string singer, int albumId)
         {
@@ -24,6 +26,27 @@ namespace WXZ8SX_HFT_2021221.Logic
                 AlbumId = albumId
             };
             _songRepository.Add(newSong);
+        }
+
+        public DateTime GetDateOfBirthOfSinger(int songId)
+        {
+            Song song = _songRepository.GetOne(songId);
+            if (song == null)
+            {
+                throw new Exception($"This song ID: {songId} does not exists!");
+            }
+            Album album = _albumRepository.GetAll().Where(album => album.AlbumId == song.AlbumId).SingleOrDefault();
+
+            Artist artist = _artistRepository.GetAll().Where(artist => artist.ArtistId == album.ArtistId).SingleOrDefault();
+
+            if (artist.DateOfBirth is DateTime artistBirthday)
+            {
+                return artistBirthday.Date;
+            }
+            else
+            {
+                throw new Exception("Invalid date of birth!");
+            }
         }
 
         public Song GetSong(int songId)
