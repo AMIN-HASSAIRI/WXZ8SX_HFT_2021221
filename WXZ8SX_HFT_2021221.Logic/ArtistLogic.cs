77637df5e9d161c886f74404eb651a1eafe5d408
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-05 18:07:21 +0100

    ArtistLogic was created and has implemented GetArtists()

---
+++ WXZ8SX_HFT_2021221.Logic/ArtistLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WXZ8SX_HFT_2021221.Models;
+using WXZ8SX_HFT_2021221.Repository;
+
+namespace WXZ8SX_HFT_2021221.Logic
+{
+    public class ArtistLogic : IArtistLogic
+    {
+        private readonly IArtistRepository _artistRepository;
+
+        public List<Artist> GetArtists()
+        {
+            return _artistRepository.GetAll().ToList();
+        }
+    }
+}
