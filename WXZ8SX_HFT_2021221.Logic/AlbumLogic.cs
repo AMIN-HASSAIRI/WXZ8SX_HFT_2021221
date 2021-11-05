67f67f27975d4a7163abf41362672c0a94185970
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-05 16:36:22 +0100

    AlbumLogic Class was created and has implemented the first method GetAlbums()

---
+++ WXZ8SX_HFT_2021221.Logic/AlbumLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WXZ8SX_HFT_2021221.Models;
+using WXZ8SX_HFT_2021221.Repository;
+
+namespace WXZ8SX_HFT_2021221.Logic
+{
+    public class AlbumLogic : IAlbumLogic
+    {
+        private readonly IAlbumRepository _albumRepository;
+
+        public List<Album> GetAlbums()
+        {
+            return _albumRepository.GetAll().ToList();
+        }
+    }
+}
