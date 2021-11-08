c3cfbd345fb5a1e516b503f6a1d587b8ca2f4f88
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-08 16:48:10 +0100

    FakeAlbumObjects() / FakeGenreObjects() were added to the GenreLogicTests

--- WXZ8SX_HFT_2021221.Test/GenreLogicTests.cs
+++ WXZ8SX_HFT_2021221.Test/GenreLogicTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WXZ8SX_HFT_2021221.Logic;
+using WXZ8SX_HFT_2021221.Models;
 
 namespace WXZ8SX_HFT_2021221.Test
 {
@@ -13,5 +14,33 @@ namespace WXZ8SX_HFT_2021221.Test
     {
         private GenreLogic AlbumLogic { get; set; }
 
+
+
+
+        private IQueryable<Album> FakeAlbumObjects()
+        {
+            Album a0 = new Album() { AlbumId = 1, AlbumName = "Test0", ReleasedDate = DateTime.Parse("03/12/1999"), NumberOfSongs = 13, GenreId = 2, ArtistId = 6, Rating = 4.8, Length = 36.66 };
+            Album a1 = new Album() { AlbumId = 2, AlbumName = "Test1", ReleasedDate = DateTime.Parse("02/21/2005"), NumberOfSongs = 9, GenreId = 3, ArtistId = 3, Rating = 3.7, Length = 27.30 };
+            Album a2 = new Album() { AlbumId = 3, AlbumName = "Test2", ReleasedDate = DateTime.Parse("03/19/1989"), NumberOfSongs = 7, GenreId = 5, ArtistId = 5, Rating = 4.8, Length = 19.00 };
+            Album a3 = new Album() { AlbumId = 4, AlbumName = "Test3", ReleasedDate = DateTime.Parse("08/13/2020"), NumberOfSongs = 5, GenreId = 1, ArtistId = 7, Rating = 3.9, Length = 15.22 };
+            Album a4 = new Album() { AlbumId = 5, AlbumName = "Test4", ReleasedDate = DateTime.Parse("04/29/2006"), NumberOfSongs = 12, GenreId = 4, ArtistId = 1, Rating = 2.0, Length = 25.20 };
+
+            List<Album> albums = new List<Album> { a0, a1, a2, a3, a4 };
+
+            return albums.AsQueryable();
+        }
+        private IQueryable<Genre> FakeGenreObjects()
+        {
+            Genre g0 = new Genre() { GenreId = 1, GenreName = "Pop" };
+            Genre g1 = new Genre() { GenreId = 2, GenreName = "Jazz" };
+            Genre g2 = new Genre() { GenreId = 3, GenreName = "RNB" };
+            Genre g3 = new Genre() { GenreId = 4, GenreName = "Techno" };
+            Genre g4 = new Genre() { GenreId = 5, GenreName = "Blues" };
+
+
+            List<Genre> genres = new List<Genre> { g0, g1, g2, g3, g4 };
+
+            return genres.AsQueryable();
+        }
     }
 }
