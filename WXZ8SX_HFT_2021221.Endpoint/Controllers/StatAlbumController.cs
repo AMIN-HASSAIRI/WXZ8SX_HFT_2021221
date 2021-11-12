8e98203c35713cb1d127b03e54c4a6bf01d92535
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-12 22:34:05 +0100

    GetTheNewestAlbum() was added to StatAlbumController

--- WXZ8SX_HFT_2021221.Endpoint/Controllers/StatAlbumController.cs
+++ WXZ8SX_HFT_2021221.Endpoint/Controllers/StatAlbumController.cs
@@ -46,5 +46,12 @@ namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
         {
             return _albumLogic.GetTheLongestAlbum();
         }
+
+        // GET stat/getthenewestalbum
+        [HttpGet]
+        public Album GetTheNewestAlbum()
+        {
+            return _albumLogic.GetTheNewestAlbum();
+        }
     }
 }
