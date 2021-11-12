d118d879e23d3face0b2da692ad9b11e490378af
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-12 22:47:55 +0100

    GetTheShortestAlbum() was added to StatAlbumController

--- WXZ8SX_HFT_2021221.Endpoint/Controllers/StatAlbumController.cs
+++ WXZ8SX_HFT_2021221.Endpoint/Controllers/StatAlbumController.cs
@@ -60,5 +60,12 @@ namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
         {
             return _albumLogic.GetTheOldestAlbum();
         }
+
+        // GET stat/gettheshortestalbum
+        [HttpGet]
+        public Album GetTheShortestAlbum()
+        {
+            return _albumLogic.GetTheShortestAlbum();
+        }
     }
 }
