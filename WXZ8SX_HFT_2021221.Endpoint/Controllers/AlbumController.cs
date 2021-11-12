7e15360e7c6ac92242241d041d0f259b60ba0991
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-12 22:15:46 +0100

    StatAlbumController was added

--- WXZ8SX_HFT_2021221.Endpoint/Controllers/AlbumController.cs
+++ WXZ8SX_HFT_2021221.Endpoint/Controllers/AlbumController.cs
@@ -19,35 +19,35 @@ namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
             _albumLogic = albumLogic;
         }
 
-        //Get Albums
+        // GET /Album
         [HttpGet]
         public IEnumerable<Album> Get()
         {
             return _albumLogic.GetAlbums();
         }
 
-        // Get Album
+        // GET /Album/3
         [HttpGet("{id}")]
         public Album Get(int id)
         {
             return _albumLogic.GetAlbum(id);
         }
 
-        // POST Album
+        // POST /Album
         [HttpPost]
         public void Post([FromBody] Album value)
         {
             _albumLogic.CreateAlbum(value.AlbumId,value.AlbumName,value.ReleasedDate,value.NumberOfSongs,value.Rating,value.Length,value.ArtistId,value.GenreId);
         }
 
-        // PUT Album
+        // PUT /Album
         [HttpPut]
         public void Put([FromBody] Album value)
         {
             _albumLogic.UpdateAlbum(value);
         }
 
-        // DELETE Album
+        // DELETE /Album/6
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
---
+++ WXZ8SX_HFT_2021221.Endpoint/Controllers/StatAlbumController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WXZ8SX_HFT_2021221.Logic;
+
+namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class StatAlbumController : ControllerBase
+    {
+        IAlbumLogic _albumLogic;
+
+        public StatAlbumController(IAlbumLogic albumLogic)
+        {
+            _albumLogic = albumLogic;
+        }
+    }
+}
