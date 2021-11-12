32e18533b46bf3734a095041aba85a182c309bfb
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-12 22:23:48 +0100

    GetAlbumsByArtist() was added to StatAlbumController

--- WXZ8SX_HFT_2021221.Endpoint/Controllers/StatAlbumController.cs
+++ WXZ8SX_HFT_2021221.Endpoint/Controllers/StatAlbumController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WXZ8SX_HFT_2021221.Logic;
+using WXZ8SX_HFT_2021221.Models;
 
 namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
 {
@@ -17,5 +18,14 @@ namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
         {
             _albumLogic = albumLogic;
         }
+
+        // GET stat/getalbumsbyartist/4
+        [HttpGet("{id}")]
+        public IEnumerable<Album> GetAlbumsByArtist(int id)
+        {
+            return _albumLogic.GetAlbumsByArtist(id);
+        }
+
+
     }
 }
