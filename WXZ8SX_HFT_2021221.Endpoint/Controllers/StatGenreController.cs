7bebf4a54bc55981d7583587a36dc074495cbfa5
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-12 23:18:03 +0100

    GetAllAlbumsWithGenre() was added to StatGenreController

--- WXZ8SX_HFT_2021221.Endpoint/Controllers/StatGenreController.cs
+++ WXZ8SX_HFT_2021221.Endpoint/Controllers/StatGenreController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WXZ8SX_HFT_2021221.Logic;
+using WXZ8SX_HFT_2021221.Models;
 
 namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
 {
@@ -16,6 +17,12 @@ namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
         public StatGenreController(IGenreLogic genreLogic)
         {
             _genreLogic = genreLogic;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Album> GetAllAlbumsWithGenre(int id)
+        {
+            return _genreLogic.GetAllAlbumsWithGenre(id);
         }
     }
 }
