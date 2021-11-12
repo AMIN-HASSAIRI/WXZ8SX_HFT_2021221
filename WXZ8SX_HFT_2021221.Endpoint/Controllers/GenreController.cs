93d5eb46355eeae7b5e8b194713c14f0f657c3b8
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-12 22:50:19 +0100

    GenreController was created

---
+++ WXZ8SX_HFT_2021221.Endpoint/Controllers/GenreController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WXZ8SX_HFT_2021221.Logic;
+
+namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class GenreController : ControllerBase
+    {
+        IGenreLogic _genreLogic;
+
+        public GenreController(IGenreLogic genreLogic)
+        {
+            _genreLogic = genreLogic;
+        }
+    }
+}
