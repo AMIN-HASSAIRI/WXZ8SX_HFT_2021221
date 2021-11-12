1dada3ddb9acfdd804a809179a82f01c1e071e36
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-12 17:57:55 +0100

    AlbumController was created

---
+++ WXZ8SX_HFT_2021221.Endpoint/Controllers/AlbumController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AlbumController : ControllerBase
+    {
+    }
+}
