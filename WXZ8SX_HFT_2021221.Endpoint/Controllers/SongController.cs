54f92cfc86b0b66215acb4481e624bbde3466b94
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-13 11:44:37 +0100

    SongController was created

---
+++ WXZ8SX_HFT_2021221.Endpoint/Controllers/SongController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WXZ8SX_HFT_2021221.Logic;
+
+namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SongController : ControllerBase
+    {
+        ISongLogic _songLogic;
+
+        public SongController(ISongLogic songLogic)
+        {
+            _songLogic = songLogic;
+        }
+    }
+}
