ec596de4e692353e89fe065a65dd9152811feda6
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-13 12:24:29 +0100

    GetWriterNameOfSong() was added to StatSongController

--- WXZ8SX_HFT_2021221.Endpoint/Controllers/StatSongController.cs
+++ WXZ8SX_HFT_2021221.Endpoint/Controllers/StatSongController.cs
@@ -60,5 +60,12 @@ namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
         {
             return _songLogic.GetSongsOrderedByName();
         }
+
+        // GET statsong/getwriternameofsong/5
+        [HttpGet("{id}")]
+        public string GetWriterNameOfSong(int id)
+        {
+            return _songLogic.GetWriterNameOfSong(id);
+        }
     }
 }
