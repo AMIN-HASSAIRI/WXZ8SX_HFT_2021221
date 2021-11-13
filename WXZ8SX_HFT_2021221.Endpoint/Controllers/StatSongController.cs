eff6860e6fa84e69bfc31961b95d2a962121dcf8
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-13 12:10:27 +0100

    GetAlbumNameOfSong() was added to StatSongController

--- WXZ8SX_HFT_2021221.Endpoint/Controllers/StatSongController.cs
+++ WXZ8SX_HFT_2021221.Endpoint/Controllers/StatSongController.cs
@@ -17,5 +17,12 @@ namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
         {
             _songLogic = songLogic;
         }
+
+        // GET statsong/getalbumnameofsong/4
+        [HttpGet("{id}")]
+        public string GetAlbumNameOfSong(int id)
+        {
+            return _songLogic.GetAlbumNameOfSong(id);
+        }
     }
 }
