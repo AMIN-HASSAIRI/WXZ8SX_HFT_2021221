776608329daab1ab41b2ba86bcc3f8dc1de54fe1
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-13 11:38:35 +0100

    GetArtistsOrderedByName() was added to StatArtistController

--- WXZ8SX_HFT_2021221.Endpoint/Controllers/StatArtistController.cs
+++ WXZ8SX_HFT_2021221.Endpoint/Controllers/StatArtistController.cs
@@ -53,5 +53,12 @@ namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
         {
             return _artistLogic.GetArtistsOrderedByBirthDate();
         }
+
+        // GET statartist/getartistsorderedbyname
+        [HttpGet]
+        public IEnumerable<Artist> GetArtistsOrderedByName()
+        {
+            return _artistLogic.GetArtistsOrderedByName();
+        }
     }
 }
