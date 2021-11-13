d005beb384e92c9aad4d22be17a8942a1f9678d1
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-13 10:41:28 +0100

    ArtistController was created

---
+++ WXZ8SX_HFT_2021221.Endpoint/Controllers/ArtistController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WXZ8SX_HFT_2021221.Logic;
+
+namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ArtistController : ControllerBase
+    {
+        IArtistLogic _artistLogic;
+
+        public ArtistController(IArtistLogic artistLogic)
+        {
+            _artistLogic = artistLogic;
+        }
+    }
+}
