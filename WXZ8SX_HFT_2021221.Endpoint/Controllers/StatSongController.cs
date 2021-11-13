18ddb92590fc39924e4d6387bacac3657d83a5a1
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-13 12:20:01 +0100

    GetSongsOrderedByLength() was added to StatSongController

--- WXZ8SX_HFT_2021221.Endpoint/Controllers/StatSongController.cs
+++ WXZ8SX_HFT_2021221.Endpoint/Controllers/StatSongController.cs
@@ -46,5 +46,12 @@ namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
         {
             return _songLogic.GetShortestSong();
         }
+
+        // GET statsong/getsongsorderedbylength
+        [HttpGet]
+        public IEnumerable<Song> GetSongsOrderedByLength()
+        {
+            return _songLogic.GetSongsOrderedByLength();
+        }
     }
 }
