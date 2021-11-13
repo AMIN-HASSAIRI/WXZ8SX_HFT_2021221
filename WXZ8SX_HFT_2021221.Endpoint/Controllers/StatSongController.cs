4b495f96a600fffdca46545e339bb170ea19cbb0
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-13 12:06:47 +0100

    StatSongController was created

---
+++ WXZ8SX_HFT_2021221.Endpoint/Controllers/StatSongController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WXZ8SX_HFT_2021221.Logic;
+
+namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class StatSongController : ControllerBase
+    {
+        ISongLogic _songLogic;
+
+        public StatSongController(ISongLogic songLogic)
+        {
+            _songLogic = songLogic;
+        }
+    }
+}
