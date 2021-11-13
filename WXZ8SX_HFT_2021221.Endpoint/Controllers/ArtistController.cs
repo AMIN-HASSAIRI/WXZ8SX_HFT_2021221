5b06c43d64a952dde97c6f1f5b814ad2f559b4e0
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-13 10:43:37 +0100

    get() was added to ArtistController

--- WXZ8SX_HFT_2021221.Endpoint/Controllers/ArtistController.cs
+++ WXZ8SX_HFT_2021221.Endpoint/Controllers/ArtistController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WXZ8SX_HFT_2021221.Logic;
+using WXZ8SX_HFT_2021221.Models;
 
 namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
 {
@@ -16,6 +17,13 @@ namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
         public ArtistController(IArtistLogic artistLogic)
         {
             _artistLogic = artistLogic;
+        }
+
+        // GET /artist
+        [HttpGet]
+        public IEnumerable<Artist> Get()
+        {
+            return _artistLogic.GetArtists();
         }
     }
 }
