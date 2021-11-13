52687f1716521b01ea489dcd3d608013f20be8e5
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-13 11:55:14 +0100

    Put() was added to SongController

--- WXZ8SX_HFT_2021221.Endpoint/Controllers/SongController.cs
+++ WXZ8SX_HFT_2021221.Endpoint/Controllers/SongController.cs
@@ -39,5 +39,12 @@ namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
         {
             _songLogic.CreateSong(value.SongId, value.Name,value.Length, value.Writer,value.Singer,value.AlbumId);
         }
+
+        // PUT /song
+        [HttpPut]
+        public void Put([FromBody] Song value)
+        {
+            _songLogic.UpdateSong(value);
+        }
     }
 }
