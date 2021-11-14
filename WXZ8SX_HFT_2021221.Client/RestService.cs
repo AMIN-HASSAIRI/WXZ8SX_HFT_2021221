444018ccd16607e035a63bb338189c743c79eb3d
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-14 08:08:10 +0100

    RestService was created in the Client Layer

---
+++ WXZ8SX_HFT_2021221.Client/RestService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WXZ8SX_HFT_2021221.Client
+{
+    class RestService
+    {
+        HttpClient client;
+
+
+    }
+}
