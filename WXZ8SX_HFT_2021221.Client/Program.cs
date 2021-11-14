c16a22450b36a089d82720f1efc71284e21aa756
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-14 15:47:04 +0100

    CreateAlbum() was added CreateGenre() was added

--- WXZ8SX_HFT_2021221.Client/Program.cs
+++ WXZ8SX_HFT_2021221.Client/Program.cs
@@ -11,11 +11,9 @@ namespace WXZ8SX_HFT_2021221.Client
         static void Main(string[] args)
         {
             System.Threading.Thread.Sleep(8000);
-
-            //RestService rest = new RestService("http://localhost:49755");
-
-            var menu = new ConsoleMenu(args, level: 0)
-               .Add(">> GET ALBUMS", () => GetAlbums())
+            var subMenuCreate = new ConsoleMenu()
+               .Add(">> CREATE ALBUM", () => CreateAlbum())
+               .Add(">> CREATE GENRE", () => CreateGenre())
                .Add("Close", ConsoleMenu.Close)
                .Add("Exit", () => Environment.Exit(0))
                .Configure(config =>
@@ -27,8 +25,21 @@ namespace WXZ8SX_HFT_2021221.Client
                    config.EnableBreadcrumb = true;
                });
 
+            //var menu = new ConsoleMenu(args, level: 0)
+            //   .Add(">> GET ALBUMS", () => GetAlbums())
+            //   .Add("Close", ConsoleMenu.Close)
+            //   .Add("Exit", () => Environment.Exit(0))
+            //   .Configure(config =>
+            //   {
+            //       config.Selector = "--> ";
+            //       config.EnableFilter = true;
+            //       config.Title = "Main menu";
+            //       config.EnableWriteTitle = true;
+            //       config.EnableBreadcrumb = true;
+            //   });
 
-            menu.Show();
+            subMenuCreate.Show();
+            //menu.Show();
 
         }
         private static void GetAlbums()
@@ -40,6 +51,65 @@ namespace WXZ8SX_HFT_2021221.Client
             }
             Console.WriteLine("Press any key to continue..");
             Console.ReadKey();
+        }
+        private static void CreateAlbum()
+        {
+            try
+            {
+                Console.WriteLine("::CREATING A NEW ALBUM::");
+                Console.WriteLine("INSERT ALBUM NAME!");
+                string name = Console.ReadLine();
+                Console.WriteLine("INSERT DATE OF RELEASED OF THE ALBUM IN THIS FORMAT (MM/dd/yyyy)!");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("INSERT NUMBER OF SONGS OF THE ALBUM!");
+                int num = int.Parse(Console.ReadLine());
+                Console.WriteLine("INSERT THE RATING OF ALBUM (MUST BE BETWEEN 0.0 AND 5.0)!");
+                double rating = double.Parse(Console.ReadLine());
+                Console.WriteLine("INSERT ARTIST ID!");
+                int artId = int.Parse(Console.ReadLine());
+                Console.WriteLine("INSERT GENRE ID!");
+                int genId = int.Parse(Console.ReadLine());
+                rest.Post<Album>(new Album() 
+                {
+                    AlbumName = name,
+                    ReleasedDate = date,
+                    NumberOfSongs = num,
+                    Rating = rating,
+                    ArtistId = artId,
+                    GenreId = genId,
+                }, "album");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+        private static void CreateGenre()
+        {
+            try
+            {
+                Console.WriteLine("::CREATE GENRE::");
+                Console.WriteLine("INSERT GENRE NAME!");
+                string name = Console.ReadLine();
+                rest.Post<Genre>(new Genre()
+                {
+                    GenreName = name,
+                }, "genre");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
         }
     }
 }
