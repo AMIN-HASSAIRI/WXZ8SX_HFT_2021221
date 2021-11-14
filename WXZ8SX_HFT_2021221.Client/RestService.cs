3127e39ae60422b811118252815abe707ce1c8d1
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2021-11-14 08:32:36 +0100

    Delete() was added to RestService

--- WXZ8SX_HFT_2021221.Client/RestService.cs
+++ WXZ8SX_HFT_2021221.Client/RestService.cs
@@ -71,5 +71,12 @@ namespace WXZ8SX_HFT_2021221.Client
 
             response.EnsureSuccessStatusCode();
         }
+        public void Delete(int id, string endpoint)
+        {
+            HttpResponseMessage response =
+                client.DeleteAsync(endpoint + "/" + id.ToString()).GetAwaiter().GetResult();
+
+            response.EnsureSuccessStatusCode();
+        }
     }
 }
