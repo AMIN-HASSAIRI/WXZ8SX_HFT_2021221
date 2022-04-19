82c782ae8bd47f74838c0b9723e1a2c93d2a413a
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2022-04-19 11:43:35 +0200

    SignalRHub was added to ArtistController

--- WXZ8SX_HFT_2021221.Endpoint/Controllers/ArtistController.cs
+++ WXZ8SX_HFT_2021221.Endpoint/Controllers/ArtistController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WXZ8SX_HFT_2021221.Endpoint.Services;
 using WXZ8SX_HFT_2021221.Logic;
 using WXZ8SX_HFT_2021221.Models;
 
@@ -13,10 +15,12 @@ namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
     public class ArtistController : ControllerBase
     {
         IArtistLogic _artistLogic;
+        IHubContext<SignalRHub> hub;
 
-        public ArtistController(IArtistLogic artistLogic)
+        public ArtistController(IArtistLogic artistLogic, IHubContext<SignalRHub> hub)
         {
             _artistLogic = artistLogic;
+            this.hub = hub;
         }
 
         // GET /artist
@@ -38,6 +42,7 @@ namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
         public void Post([FromBody] Artist value)
         {
             _artistLogic.CreateArtist(value);
+            hub.Clients.All.SendAsync("ArtistCreated", value);
         }
 
         // PUT /artist
@@ -45,13 +50,16 @@ namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
         public void Put([FromBody] Artist value)
         {
             _artistLogic.UpdateArtist(value);
+            hub.Clients.All.SendAsync("ArtistUpdated", value);
         }
 
         // DELETE /artist/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var artistToDelete = this._artistLogic.GetArtist(id);
             _artistLogic.RemoveArtist(id);
+            hub.Clients.All.SendAsync("ArtistDeleted", artistToDelete);
         }
     }
 }
