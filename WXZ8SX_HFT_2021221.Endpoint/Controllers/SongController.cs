e9d3248587d2e6e517b431f2f94904a6a8c72dbd
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2022-04-19 11:47:59 +0200

    SignalRHub was added to SongController

--- WXZ8SX_HFT_2021221.Endpoint/Controllers/SongController.cs
+++ WXZ8SX_HFT_2021221.Endpoint/Controllers/SongController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WXZ8SX_HFT_2021221.Endpoint.Services;
 using WXZ8SX_HFT_2021221.Logic;
 using WXZ8SX_HFT_2021221.Models;
 
@@ -13,10 +15,12 @@ namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
     public class SongController : ControllerBase
     {
         ISongLogic _songLogic;
+        IHubContext<SignalRHub> hub;
 
-        public SongController(ISongLogic songLogic)
+        public SongController(ISongLogic songLogic, IHubContext<SignalRHub> hub)
         {
             _songLogic = songLogic;
+            this.hub = hub;
         }
 
         // GET /song
@@ -38,6 +42,7 @@ namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
         public void Post([FromBody] Song value)
         {
             _songLogic.CreateSong(value);
+            hub.Clients.All.SendAsync("SongCreated", value);
         }
 
         // PUT /song
@@ -45,13 +50,16 @@ namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
         public void Put([FromBody] Song value)
         {
             _songLogic.UpdateSong(value);
+            hub.Clients.All.SendAsync("SongUpdated", value);
         }
 
         // DELETE /song/2
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var songToDelete = this._songLogic.GetSong(id);
             _songLogic.RemoveSong(id);
+            hub.Clients.All.SendAsync("GenreDeleted", songToDelete);
         }
     }
 }
