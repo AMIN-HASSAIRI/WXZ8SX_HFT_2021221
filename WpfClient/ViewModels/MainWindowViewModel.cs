6ae8ab71f70fd38119a161a392185f02bacc560b
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2022-04-19 12:17:33 +0200

    MainWindowViewModel was added

---
+++ WpfClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfClient.ViewModels
+{
+    public class MainWindowViewModel : ObservableRecipient
+    {
+        public RelayCommand ManageAlbumsCommand { get; set; }
+        public RelayCommand ManageArtistsCommand { get; set; }
+        public RelayCommand ManageGenreCommand { get; set; }
+        public RelayCommand ManageSongsCommand { get; set; }
+
+        public RelayCommand ManageNonCrudAlbumsCommand { get; set; }
+        public RelayCommand ManageNonCrudArtistsCommand { get; set; }
+        public RelayCommand ManageNonCrudGenreCommand { get; set; }
+        public RelayCommand ManageNonCrudSongsCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
