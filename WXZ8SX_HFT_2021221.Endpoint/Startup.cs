497f304c7c281efb9799c33cba94337ec1307488
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2022-04-19 11:58:04 +0200

    Edit on Configure in the Startup.cs

--- WXZ8SX_HFT_2021221.Endpoint/Startup.cs
+++ WXZ8SX_HFT_2021221.Endpoint/Startup.cs
@@ -1,13 +1,16 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WXZ8SX_HFT_2021221.Data;
+using WXZ8SX_HFT_2021221.Endpoint.Services;
 using WXZ8SX_HFT_2021221.Logic;
 using WXZ8SX_HFT_2021221.Repository;
 
@@ -35,6 +38,15 @@ namespace WXZ8SX_HFT_2021221.Endpoint
 
             services.AddTransient<MusicDbContext, MusicDbContext>();
 
+            //new
+            services.AddSignalR();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MusicDbApp.Endpoint", Version = "v1" });
+            });
+            //
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,11 +57,29 @@ namespace WXZ8SX_HFT_2021221.Endpoint
                 app.UseDeveloperExceptionPage();
             }
 
+            //new
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features
+                    .Get<IExceptionHandlerPathFeature>()
+                    .Error;
+                var response = new { Msg = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
+            //
+
             app.UseRouting();
+
+            //new
+            app.UseAuthorization();
+            //
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                //new
+                endpoints.MapHub<SignalRHub>("/hub");
+                //
             });
         }
     }
