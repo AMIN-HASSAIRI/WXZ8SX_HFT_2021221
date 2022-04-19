e31aa312bb55aff04030da8f8ae1bd80a1897849
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2022-04-19 11:52:18 +0200

    SignalRHub was added to StatGenreController

--- WXZ8SX_HFT_2021221.Endpoint/Controllers/StatGenreController.cs
+++ WXZ8SX_HFT_2021221.Endpoint/Controllers/StatGenreController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WXZ8SX_HFT_2021221.Endpoint.Services;
 using WXZ8SX_HFT_2021221.Logic;
 using WXZ8SX_HFT_2021221.Models;
 
@@ -13,10 +15,12 @@ namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
     public class StatGenreController : ControllerBase
     {
         IGenreLogic _genreLogic;
+        IHubContext<SignalRHub> hub;
 
-        public StatGenreController(IGenreLogic genreLogic)
+        public StatGenreController(IGenreLogic genreLogic, IHubContext<SignalRHub> hub)
         {
             _genreLogic = genreLogic;
+            this.hub = hub;
         }
 
         // GET statgenre/getallalbumswithgenre/4
