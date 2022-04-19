29f0e1b47b4ce51933108eaa8a2757e87964f1b7
AMIN-HASSAIRI <92026336+AMIN-HASSAIRI@users.noreply.github.com>
2022-04-19 11:41:47 +0200

    SignalRHub was added to AlbumController

--- WXZ8SX_HFT_2021221.Endpoint/Controllers/AlbumController.cs
+++ WXZ8SX_HFT_2021221.Endpoint/Controllers/AlbumController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WXZ8SX_HFT_2021221.Endpoint.Services;
 using WXZ8SX_HFT_2021221.Logic;
 using WXZ8SX_HFT_2021221.Models;
 
@@ -14,11 +15,12 @@ namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
     public class AlbumController : ControllerBase
     {
         IAlbumLogic _albumLogic;
-        //IHubContext<SignalRHub> hub;
+        IHubContext<SignalRHub> hub;
 
-        public AlbumController(IAlbumLogic albumLogic)
+        public AlbumController(IAlbumLogic albumLogic, IHubContext<SignalRHub> hub)
         {
             _albumLogic = albumLogic;
+            this.hub = hub;
         }
 
         // GET /album
@@ -40,6 +42,7 @@ namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
         public void Post([FromBody] Album value)
         {
             _albumLogic.CreateAlbum(value);
+            hub.Clients.All.SendAsync("AlbumCreated", value);
         }
 
         // PUT /album
@@ -47,13 +50,16 @@ namespace WXZ8SX_HFT_2021221.Endpoint.Controllers
         public void Put([FromBody] Album value)
         {
             _albumLogic.UpdateAlbum(value);
+            hub.Clients.All.SendAsync("AlbumUpdated", value);
         }
 
         // DELETE /album/6
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var albumToDelete = this._albumLogic.GetAlbum(id);
             _albumLogic.RemoveAlbum(id);
+            hub.Clients.All.SendAsync("AlbumDeleted", albumToDelete);
         }
     }
 }
